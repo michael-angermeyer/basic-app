fd3622540866a85eadae542e14764f96de9be46e
michael-angermeyer <michael-angermeyer@hotmail.de>
2023-08-23 13:45:02 +0200

    updated

--- src/App.js
+++ src/App.js
@@ -1,21 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const update = () => {
+    window.location.reload();
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+        <p>Whirlpool Temperatur: 32 °C</p>
+        <p>Ziel Temperatur: 38 °C</p>
+        <a href="/" className="App-button" onClick={update}>
+          Aktualisieren
         </a>
       </header>
     </div>
